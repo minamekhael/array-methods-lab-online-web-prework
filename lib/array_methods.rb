d358f5164aba45f3522f23b14eac33bc1b1aed3b
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-05 23:31:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,18 +8,3 @@ def using_sort(array)
   array = [0, 1, 2, 3, 4]
   puts array.sort
 end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-end
-
-def using_size(array)
-
-end
